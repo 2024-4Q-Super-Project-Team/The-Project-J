ca927472474db12aa1dba66d433f79e7d6b06a1b
PPAK_JU <taku7664@gmail.com>
2025-01-19 03:33:54 +0900

    리소스 구조 개선 전 커밋

--- MyD3D/Editor/CumstomWidget/TabItem/Viewer/ResourceViewer/EditorResourceViewer.cpp
+++ MyD3D/Editor/CumstomWidget/TabItem/Viewer/ResourceViewer/EditorResourceViewer.cpp
@@ -19,6 +19,18 @@ void Editor::ResourceViewer::Render()
 		std::string ResourceName = ResourceTypeToStr((eResourceType)i);
 		if (ImGui::TreeNodeEx((ResourceName).c_str(), ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_Selected))
 		{
+			static char buffer[128] = "";
+			const char* defaultPath = "resource/";
+			strcpy_s(buffer, defaultPath);
+
+			ImGui::Text(("Add Resource " + ResourceName).c_str());
+			if (ImGui::InputText(("##Add_" + ResourceName).c_str(), buffer, sizeof(buffer), ImGuiInputTextFlags_EnterReturnsTrue))
+			{
+				const std::wstring resourcePath = Helper::ToWString(std::string(buffer));
+			}
+
+			ImGui::Separator();
+
 			ResourceTable& table = ResourceManager::GetResourceTable((eResourceType)i);
 			for (auto& resource : table)
 			{

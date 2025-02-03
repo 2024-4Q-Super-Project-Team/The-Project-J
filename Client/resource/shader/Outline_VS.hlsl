f23661997e3f52c4926341b821a65531b2b692ae
rkdms <fmsl10041004@naver.com>
2025-02-03 20:37:47 +0900

    Outline_VS 수정

--- Client/resource/shader/Outline_VS.hlsl
+++ Client/resource/shader/Outline_VS.hlsl
@@ -4,12 +4,16 @@ OUTLINE_VS_OUTPUT main(STD_VS_INPUT input)
 {
     OUTLINE_VS_OUTPUT output;
     
-    float3 outNormal = normalize(mul(input.normal, (float3x3) WorldMatrix)) * outlineOffset;
-    float3 outlinePos = input.pos.xyz + outNormal;
-
-    float4 worldPos = mul(float4(outlinePos, 1.f), WorldMatrix);
+    float3 outNormal = normalize(mul(input.normal, (float3x3) WorldMatrix));
+    
+    float4 worldPos = mul(float4(input.pos.xyz, 1.f), WorldMatrix);
+    
+    float distToCam = length(CameraPosition.xyz - worldPos.xyz);
+    
+    worldPos.xyz += outNormal * outlineOffset * 2.f * distToCam;
+    
     float4 viewPos = mul(worldPos, ViewMatrix);
     output.pos = mul(viewPos, ProjectionMatrix);
-   
+    
     return output;
 }

fb817bb839449c9552dfeddeb98db40646eaa626
PPAK_JU <taku7664@gmail.com>
2025-02-05 23:28:28 +0900

    CameraScript(1)

--- Client/Contents/GameApp/Script/CameraController.h
+++ Client/Contents/GameApp/Script/CameraController.h
@@ -15,10 +15,8 @@ public:
     SerializeField(FLOAT, sensitivity, 5.0f);
     SerializeField(FLOAT, maxYAngle, 3.0f);
     FLOAT CurrentAngles[3];
-    FLOAT CurrentSubCameraAngles[3];
     Transform* mTr;
     Camera* mCamera;
-    Camera* mSubCamera;
 public:
     enum AxisType
     {

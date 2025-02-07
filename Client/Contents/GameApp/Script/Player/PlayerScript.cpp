13515b084e4005595de1e7e9e07dee16044f46d2
PPAK_JU <taku7664@gmail.com>
2025-02-07 17:29:45 +0900

    BossScript 기본 틀 구성 및 PlayerScript QA사항 적용....

--- Client/Contents/GameApp/Script/Player/PlayerScript.cpp
+++ Client/Contents/GameApp/Script/Player/PlayerScript.cpp
@@ -111,7 +111,7 @@ void PlayerScript::OnCollisionStay(Rigidbody* _origin, Rigidbody* _destination)
     BurnObjectScript* dstBurnObject = _destination->gameObject->GetComponent<BurnObjectScript>();
     if (dstBurnObject)
         Display::Console::Log(L"Can Fire \n");
-    if (InputSyncer::IsKeyDown(mPlayerHandle.val, InputSyncer::MOVE_FIRE))
+    if (isJump == false && InputSyncer::IsKeyHold(mPlayerHandle.val, InputSyncer::MOVE_FIRE))
     {
         ProcessMoveFire(dstBurnObject);
         return;
@@ -136,7 +136,7 @@ void _CALLBACK PlayerScript::OnTriggerStayCallback(Collider* _origin, Collider* 
     ////////////////////////////////////////////////
     // BurnObjectScript를 GetComponent성공했냐로 대상이 불을 옮길 수 있는 오브젝트 인가를 구분
     BurnObjectScript* dstBurnObject = _destination->gameObject->GetComponent<BurnObjectScript>();
-    if (isJump == false && InputSyncer::IsKeyDown(mPlayerHandle.val, InputSyncer::MOVE_FIRE))
+    if (isJump == false && InputSyncer::IsKeyHold(mPlayerHandle.val, InputSyncer::MOVE_FIRE))
     {
         ProcessMoveFire(dstBurnObject);
         return;
@@ -323,7 +323,7 @@ void PlayerScript::UpdateMoveFire()
         if (mBurnProcessTarget)
         {
             // 중간에 키를 떼면 취소한다.
-            if (InputSyncer::IsKeyUp(mPlayerHandle.val, InputSyncer::MOVE_FIRE))
+            if (false == InputSyncer::IsKeyHold(mPlayerHandle.val, InputSyncer::MOVE_FIRE))
             {
                 SetState(ePlayerStateType::IDLE);
                 mBurnProcessTarget = nullptr;
@@ -357,7 +357,6 @@ void PlayerScript::UpdateMoveFire()
                     mBurnObjectScript->IsBurning() == true)
                 {
                     mBurnProcessTarget->SetBurn(true);
-                    mBurnObjectScript->SetBurn(false);
                 }
                 mBurnProcessTarget = nullptr;
                 mMoveFireCount = 0.0f;
@@ -451,7 +450,9 @@ void PlayerScript::ProcessMoveFire(BurnObjectScript* _dst)
     if (_dst == nullptr || _dst == mBurnObjectScript) return;
 
     // 대상이 꺼져있고 내가 불타는 중에만 동작한다.
-    if (_dst->IsBurning() == false && mBurnObjectScript->IsBurning() == true)
+    if (_dst->IsBurning() == false && 
+        mBurnObjectScript->IsBurning() == true &&
+        mPlayerState != ePlayerStateType::MOVE_FIRE)
     {
         SetState(ePlayerStateType::MOVE_FIRE);
         mBurnProcessTarget = _dst;

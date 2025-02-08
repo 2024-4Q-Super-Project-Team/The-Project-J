83372f6580c639aaae11dcbacb3fa8b04d4c7961
Jiwon Choi <84387145+gamzachips@users.noreply.github.com>
2025-02-09 06:52:03 +0900

    box raycast 됨

--- Client/Contents/GameApp/Script/Object/Box/BoxScript.cpp
+++ Client/Contents/GameApp/Script/Object/Box/BoxScript.cpp
@@ -9,10 +9,23 @@ void BoxScript::Start()
     mRigid->SetMass(15.f);
 
     mCollider->SetExtents(Vector3(70, 75, 70));
-    mCollider->SetPosition(Vector3(0, 40, 0));
+    mCollider->SetPosition(Vector3(0, 35, 0));
 
     mPxRayDirection = PxVec3(0, -1, 0);
 
+    // 부모의 회전 행렬 가져오기 (부모의 월드 변환 행렬에서 회전 부분만 추출)
+    Matrix parentWorldMatrix = gameObject->transform->GetParent()->GetWorldMatrix();
+    Matrix parentRotationMatrix = Matrix::CreateFromQuaternion(Quaternion::CreateFromRotationMatrix(parentWorldMatrix));
+
+    if (mPxRayDirection.magnitudeSquared() > 0.f)
+        mPxRayDirection.normalize();
+
+    Vector3 dir = Vector3(mPxRayDirection.x, mPxRayDirection.y, mPxRayDirection.z);
+
+    XMVECTOR transformedDirection = XMVector3TransformNormal(dir, parentRotationMatrix);
+    Vector3 direction = Vector3(transformedDirection);
+
+    mPxRayDirection = PxVec3(direction.x, direction.y, direction.z);
 }
 
 void BoxScript::Update()
@@ -20,7 +33,7 @@ void BoxScript::Update()
    if (mRigid == nullptr) return;
    
    //중력을 주어야 하는지를 판단하기 위해 바닥으로 쏘는 ray 
-   Vector3 rayOriginPos = gameObject->transform->GetWorldPosition() + Vector3(0, -1.0f, 0);
+   Vector3 rayOriginPos = gameObject->transform->GetWorldPosition() + Vector3(0, -0.5f, 0);
    mPxRayOrigin = PxVec3(rayOriginPos.x, rayOriginPos.y, rayOriginPos.z);
 
    PxQueryFilterData filterData(PxQueryFlag::eSTATIC);
@@ -29,9 +42,7 @@ void BoxScript::Update()
    mGravityOn.val = !mHitBuffer.hasBlock;
 
    if (mGravityOn.val)
-       displacement.val.y -= mGravitySpeed;
-   else
-       displacement.val.y = 0;
+       displacement.val.y = -mGravitySpeed;
 
 
    MoveBox(displacement.val * Time::GetScaledDeltaTime());
@@ -41,23 +52,23 @@ void BoxScript::OnCollisionEnter(Rigidbody* box, Rigidbody* player)
 {
     if (player->gameObject->GetTag() != L"Player") return;
 
-        Vector3 playerPos = player->gameObject->transform->GetWorldPosition();
-        Vector3 boxPos = box->gameObject->transform->GetWorldPosition();
-        Vector3 localDirection = -(boxPos - playerPos);
+    Vector3 playerPos = player->gameObject->transform->GetWorldPosition();
+    Vector3 boxPos = box->gameObject->transform->GetWorldPosition();
+    Vector3 localDirection = -(boxPos - playerPos);
 
-        // 부모의 회전 행렬 가져오기 (부모의 월드 변환 행렬에서 회전 부분만 추출)
-        Matrix parentWorldMatrix = box->gameObject->transform->GetParent()->GetWorldMatrix();
-        Matrix parentRotationMatrix = Matrix::CreateFromQuaternion(Quaternion::CreateFromRotationMatrix(parentWorldMatrix));
+    // 부모의 회전 행렬 가져오기 (부모의 월드 변환 행렬에서 회전 부분만 추출)
+    Matrix parentWorldMatrix = box->gameObject->transform->GetParent()->GetWorldMatrix();
+    Matrix parentRotationMatrix = Matrix::CreateFromQuaternion(Quaternion::CreateFromRotationMatrix(parentWorldMatrix));
 
-        localDirection.y = 0.f;
-        if (localDirection.LengthSquared() > 0.f)
-            localDirection.Normalize();
+    localDirection.y = 0.f;
+    if (localDirection.LengthSquared() > 0.f)
+        localDirection.Normalize();
 
 
-        XMVECTOR transformedDirection = XMVector3TransformNormal(localDirection, parentRotationMatrix);
-        Vector3 direction = Vector3(transformedDirection);
+    XMVECTOR transformedDirection = XMVector3TransformNormal(localDirection, parentRotationMatrix);
+    Vector3 direction = Vector3(transformedDirection);
 
-        displacement.val = direction * mMoveSpeed;
+    displacement.val = direction * mMoveSpeed;
 
 }
 
